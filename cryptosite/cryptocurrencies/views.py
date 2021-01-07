ca447241ad740f0578309aa4e22959a5187a8099
vlad101 <vladimir.efros@gmail.com>
2021-01-07 15:17:21 -0500

    implemented visitor counter of page visits through session counter

--- cryptosite/cryptocurrencies/views.py
+++ cryptosite/cryptocurrencies/views.py
@@ -12,6 +12,15 @@ class IndexView(generic.ListView):
         """Return all coins."""
         return Coin.objects.all()
 
+    def get_context_data(self, **kwargs):
+        # Number of visits to the index page, as counted in the session variable.
+        num_visits = self.request.session.get('num_visits', 1)
+        self.request.session['num_visits'] = num_visits + 1
+        # Build context data
+        context = super().get_context_data(**kwargs)
+        context['num_visits'] = num_visits
+        return context
+
 class DetailView(generic.DetailView):
     model = Coin
     template_name = 'cryptocurrencies/detail.html'
