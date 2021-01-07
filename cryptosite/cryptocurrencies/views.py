b3b1a80c69d2e2cfcd76606bf3ea5033908f18f3
vlad101 <vladimir.efros@gmail.com>
2021-01-07 12:28:59 -0500

    cryptos list url bug fix, added basic static

--- cryptosite/cryptocurrencies/views.py
+++ cryptosite/cryptocurrencies/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views import generic
 
 from .models import Coin, CoinData
 
 class IndexView(generic.ListView):
-    template_name = 'cryptocurrencies/index.html'
     context_object_name = 'coin_list'
+    template_name = 'cryptocurrencies/index.html'
 
     def get_queryset(self):
-        """Return all crypto ccoins."""
-        return Coins.objects.all()
+        """Return all coins."""
+        return Coin.objects.all()
 
 class DetailView(generic.DetailView):
     model = Coin
